2b9569b4a01ca481e98cb8bae69c292016a20ae8
Sean Gowing <twistedx@users.noreply.github.com>
2022-06-05 13:42:47 -0700

    adding code

---
+++ postMessageListener.js
@@ -0,0 +1,36 @@
+//This code was obtained from Julian at MeasureSchool.
+
+(function (window) {
+  addEvent(window, "message", function (message) {
+    try {
+      var data = JSON.parse(message.data);
+      var dataLayer = window.dataLayer || (window.dataLayer = []);
+      if (data.event) {
+        dataLayer.push({
+          event: data.event,
+          postMessageData: data,
+        });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  // Cross-browser event listener
+  function addEvent(el, evt, fn) {
+    if (el.addEventListener) {
+      el.addEventListener(evt, fn);
+    } else if (el.attachEvent) {
+      el.attachEvent("on" + evt, function (evt) {
+        fn.call(el, evt);
+      });
+    } else if (
+      typeof el["on" + evt] === "undefined" ||
+      el["on" + evt] === null
+    ) {
+      el["on" + evt] = function (evt) {
+        fn.call(el, evt);
+      };
+    }
+  }
+})(window);
